2f4df35f3b8097b4d730a94d18e79274bf47ef36
LudoBoggio <Ludo.Boggio+GitHub@GMail.com>
2012-09-18 15:38:07 +0200

    v1.3

--- README.md
+++ README.md
@@ -6,13 +6,13 @@ This plugin is intended to be used with YOURLS (cf. http://yourls.org)
 
 It has been tested on YOURLS v1.5 and v1.5.1
 
-Current version is 1.2, updated on 11/09/2012
+Current version is 1.3, updated on 18/09/2012
 
 Contact : Ludo at Ludo.Boggio+GitHub@GMail.com
 
 INSTALL :
 - In /user/plugins, create a new folder named BlackListIP
-- In this new directory, copy the plugin.php file from this repository
+- In this new directory, copy the plugin.php and ludo_blacklist_ip_Check_IP_Module.php files from this repository
 - Go to the Plugins administration page and activate the plugin
 
 You will see in the admin section a new admin page where you can add the IP addresses you want to blacklist.
@@ -22,5 +22,14 @@ Please enter one IP address per line. Other syntax should provide unexpected beh
 v1.0 : initialization
 v1.1 : Add admin page
 v1.2 : Add some checks on IP format and some warnings for use
+v1.3 : Add several possibilities to provide IP ranges :
+       - A.B.C.D-X.Y.Z.T range : all IPs from A.B.C.D to X.Y.Z.T are blacklisted
+	   - A.B.C.0 range : all IPs from A.B.C.0 to A.B.C.255 are blacklisted
+	   - A.B.0.0 range : all IPs from A.B.0.0 to A.B.255.255 are blacklisted
+	   - A.0.0.0 range : all IPs from A.0.0.0 to A.255.255.255 are blacklisted
+	   - A.B.C.D/X.Y.Z.T : A.B.C.D is an IP address, X.Y.Z.T is a subnet mask, all IPs addresses corresponding to that IP and mask are blacklisted
+	   - A.B.C.D/T, T between 0 TO 32 : CIDR notation.
+	   
+	   For explanations, feel free to check http://en.wikipedia.org/wiki/IP_address .
 
-Roadmap contains the possibility to use regexp to specify the IP addresses. No, no date provided :)
+Actual roadmap is empty, but I'm open to suggestions. Feel free to contact me.
--- plugin.php
+++ plugin.php
@@ -11,6 +11,8 @@ Author URI: http://ludo.boggio.fr
 // No direct call
 if( !defined( 'YOURLS_ABSPATH' ) ) die();
 
+include "ludo_blacklist_ip_Check_IP_Module.php";
+
 // Hook the custom function into the 'pre_check_ip_flood' event
 yourls_add_action( 'pre_check_ip_flood', 'ludo_blacklist_ip_root' );
 
@@ -19,15 +21,17 @@ yourls_add_action( 'plugins_loaded', 'ludo_blacklist_ip_add_page' );
 
 // Get blacklisted IPs from YOURLS options feature and compare with current IP address
 function ludo_blacklist_ip_root ( $args ) {
-    $ip = $args[0];
-    $liste_ip = yourls_get_option ('ludo_blacklist_ip_liste');
-    if ( $liste_ip !== false ) {
-        $liste_ip_display = unserialize ( $liste_ip );
-        if (in_array($ip, $liste_ip_display)) {
-    //      yourls_die ( "Your IP has been blacklisted.", "Black list",403);
-            echo "<center>Your IP has been blacklisted.</center>";
-            die();
-        }
+	$IP = $args[0];
+	$Intervalle_IP = unserialize ( yourls_get_option ('ludo_blacklist_ip_liste') );
+
+	foreach ( $Intervalle_IP as $value ) {
+		$IPs = explode ( "-" , $value );
+
+		if ( $IP >= $IPs[0] AND $IP <= $IPs[1]) {
+			//      yourls_die ( "Your IP has been blacklisted.", "Black list",403);
+			echo "<center>Your IP has been blacklisted.</center>";
+			die();
+		}
     }
 }
 // Add admin page
@@ -48,7 +52,7 @@ function ludo_blacklist_ip_do_page () {
 function ludo_blacklist_ip_form () {
     $nonce = yourls_create_nonce( 'blacklist_ip' ) ;
     $liste_ip = yourls_get_option ('ludo_blacklist_ip_liste','Enter IP addresses here, one per line');
-    if ($liste_ip != 'Enter IP addresses here, one per line' )
+    if ($liste_ip != 'Enter IP addresses here, one entry per line' )
         $liste_ip_display = implode ( "\r\n" , unserialize ( $liste_ip ) );
 	else
 		$liste_ip_display=$liste_ip;
@@ -64,6 +68,14 @@ function ludo_blacklist_ip_form () {
         
         <p><input type="submit" value="Save" /></p>
 		<p>I suggest to add here IPs that you saw adding bulk URL. It is your own responsibility to check the use of the IPs you block. WARNING : erroneous entries may create unexpected behaviours, please double-check before validation.</p>
+		<p>Follwing formats are accepted : 
+			<ul>
+				<li>A.B.C.D/X : A.B.C.D is an IP address, X is the bit number for the mask (CIDR notation).</li>
+				<li>A.B.C.D/X.Y.Z.T : A.B.C.D is an IP address, X.Y.Z.T is the subnet mask.</li>
+				<li>A.B.C.D-X.Y.Z.T : range from IP address A.B.C.D to IP address X.Y.Z.T (included)</li>
+				<li>A.B.C.D : if D, C.D, B.C.D are only 0 : cover all the subnet included in that range. if there are no 0 at the end, specify only this IP address</li>
+			</ul>
+		</p>
         </form>
 HTML;
 }
@@ -80,26 +92,24 @@ function ludo_blacklist_ip_process () {
 		echo "Bad answer, Blacklist not updated";
 		die ();
 	}
-
-	foreach ($IP_Form as $key => $value)
-		if ( !ludo_blacklist_check_IP ( $value) ) unset ($IP_Form [$key]) ;
-		
-
-	// Update list
-	$sent_list = serialize ( $IP_Form );
-	yourls_update_option ( 'ludo_blacklist_ip_liste', $sent_list );
-	echo "Black list updated. New blacklist is :<BR />" ;
-	foreach ($IP_Form as $value) echo $value."<BR />";
-}
 	
-function ludo_blacklist_check_IP ($IP) {
-	$IPs = explode ( "." , $IP );
-//echo "Testing ".$IP."<BR />";
-	if (count ($IPs) != 4 ) return false ;
-	foreach ( $IPs as $value ) 
-		if (! ctype_digit($value) || $value < 0 || $value > 255 )
-			return false ;
-	return true;
+	$boucle = 0;
+
+	foreach ($IP_Form as $value) {
+		$Retour = ludo_blacklist_ip_Analyze_IP ( $value ) ;
+		if ( $Retour != "NULL" ) {
+			$IPList[$boucle++] = $Retour ;
+		}
+	}
+	// Update list
+	yourls_update_option ( 'ludo_blacklist_ip_liste', serialize ( $IPList ) );
+	echo "Black list updated. New blacklist is " ;
+	if ( count ( $IPList ) == 0 ) 
+		echo "empty.";
+	else {
+		echo ":<BR />";
+		foreach ($IPList as $value) echo $value."<BR />";
+	}
 }
 
 
